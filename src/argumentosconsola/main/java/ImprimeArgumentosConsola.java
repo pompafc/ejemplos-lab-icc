573ad471bcd97661eff186ca5b482468b4dfad90
Isaac Pompa <isaacsalvador18@ciencias.unam.mx>
2024-10-06 23:58:48 -0600

    Commit inicial
    Ejemplificar los argumentos del main de Java.

---
+++ src/argumentosconsola/main/java/ImprimeArgumentosConsola.java
@@ -0,0 +1,14 @@
+package argumentosconsola.main.java;
+
+public class ImprimeArgumentosConsola {
+
+	public static void main(String[] args) {
+
+	  String arg0 = args[0];
+          String arg1 = args[1];
+
+          System.out.println("El argumento 1 es:" + arg0);	  
+          System.out.println("El argumento 2 es:" + arg1);	  
+	
+	}
+}
